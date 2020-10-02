bb7d1c183d530516e6eb320b36bc7f0cdacf5956
momoshiro407 <rozabmmsr@gmail.com>
2020-10-02 21:53:12 +0900

    Paper.jsを利用したプロット処理、多角形面積計算処理を実装

--- src/app/routed-modules/plotter/components/plot-area/plot-area.component.ts
+++ src/app/routed-modules/plotter/components/plot-area/plot-area.component.ts
@@ -13,10 +13,12 @@ export class PlotAreaComponent implements OnInit {
   canvas: ElementRef<HTMLCanvasElement>;
 
   path: any;
+  unsettledPath: any;
   currentX: number;
   currentY: number;
   vertexList: Vertex[] = [];
   polygonArea: number;
+  isCross = false;
 
   constructor() { }
 
@@ -28,14 +30,20 @@ export class PlotAreaComponent implements OnInit {
     const rect = event.target.getBoundingClientRect();
     this.currentX = event.clientX - rect.left;
     this.currentY = event.clientY - rect.top;
+    this.drawUnsettledLine();
   }
 
   onClickCanvas(): void {
+    if (this.isCross) {
+      alert('パスが交差する位置に点を打つことは出来ません。');
+      return;
+    }
     // 多角形の面積が計算されている=パスが閉じられている時はプロットできないようにする
     if (this.polygonArea) { return; }
     // プロットする前に予め空のPathオブジェクトを生成する
     if (this.vertexList.length === 0) {
       this.path = new Path();
+      this.unsettledPath = new Path();
     }
     // パスの頂点座標の配列にクリック位置のx, y座標を追加する
     this.vertexList.push({
@@ -51,6 +59,7 @@ export class PlotAreaComponent implements OnInit {
     if (this.vertexList.length === 0) { return; }
     this.path.closePath();
     this.path.fillColor = 'rgb(255, 0, 0, 0.2)';
+    this.unsettledPath.removeSegments();
     this.calculatePolygonArea();
   }
 
@@ -58,6 +67,7 @@ export class PlotAreaComponent implements OnInit {
     paper.project.activeLayer.removeChildren();
     this.polygonArea = null;
     this.vertexList = [];
+    this.isCross = false;
   }
 
   calculatePolygonArea(): void {
@@ -82,5 +92,28 @@ export class PlotAreaComponent implements OnInit {
     this.path.strokeColor = 'rgb(255, 0, 0)';
     this.path.strokeWidth = 2;
     this.path.add(new Point(this.currentX, this.currentY));
+    this.unsettledPath.removeSegments();
+  }
+
+  private drawUnsettledLine(): void {
+    if (!this.path || !this.unsettledPath || this.polygonArea) { return; }
+    this.unsettledPath.removeSegments();
+    // 未確定パスの設定
+    this.unsettledPath.strokeColor = 'rgb(0, 0, 0, 0.1)';
+    this.unsettledPath.strokeWidth = 1;
+    // 確定パスの最先端にある頂点座標を取得する
+    const lastSegment = this.path.lastSegment.point;
+    // 未確定パスの始点
+    this.unsettledPath.add(new Point(lastSegment.x, lastSegment.y));
+    // 未確定パスの終点
+    this.unsettledPath.add(new Point(this.currentX, this.currentY));
+
+    // 確定パスと未確定パスの交差を判定する
+    this.checkCrossing();
+  }
+
+  private checkCrossing(): void {
+    const interSection = this.path.getIntersections(this.unsettledPath);
+    this.isCross = interSection.length > 1;
   }
 }
